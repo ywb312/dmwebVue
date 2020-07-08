f8fd3e40c79e98146879197cd78310c95c4770ef
yuwenbin <792345263@qq.com>
2020-07-08 10:08:10 +0800

    fix:风险公告栏、告知卡数据合并

--- src/components/risk/informCard.vue
+++ src/components/risk/informCard.vue
@@ -7,20 +7,28 @@
                         <h4>{{index+1+'.'+item.name}}</h4>
                     </div>
                     <div class="main">
-                        <div>
-                            <p>作业步骤: {{item.project}}</p>
-                        </div>
-                        <div>
-                            <p>事故类别: {{item.knfsText}}</p>
-                        </div>
-                        <div>
-                            <p>危害有害因素: {{item.wname}}</p>
-                        </div>
-                        <div>
-                            <p>责任单位: {{item.deptname}}</p>
-                        </div>
-                        <div>
-                            <p>管控措施: {{item.gtypeText}}</p>
+                        <div class="noFlex" v-for="(n,m) in item.child" :key="m">
+                            <div>
+                                <p>
+                                    <span class="main_title bold">{{"("+(m+1)+")"}}</span>
+                                    <span class="main_val bold">{{n.deptname}}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>作业步骤: {{item.project}}</p>
+                            </div>
+                            <div>
+                                <p>事故类别: {{item.knfsText}}</p>
+                            </div>
+                            <div>
+                                <p>危害有害因素: {{item.wname}}</p>
+                            </div>
+                            <div v-for="(key,val) in n.child" :key="val">
+                                <p>
+                                    <span class="main_title">{{key.gtypeText+":"}}</span>
+                                    <span class="main_val">{{key.gname}}</span>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,15 +55,58 @@ export default {
         // 处理请求的数据
         getRendering(arr) {
             let _self = this;
-            Promise.all([
+            this.setRes(arr).then(res => {
+                _self.rendering = res;
+            });
+        },
+        // 设置返还参数
+        setRes(arr) {
+            let _self = this;
+            let returnArr = [];
+            function getItem(key, val, arr) {
+                return arr.findIndex(item => item[key] == val);
+            }
+            return Promise.all([
                 _self.$common.comboList({ sourcename: "KNFS" }),
                 _self.$common.comboList({ sourcename: "GKCSLX" })
             ]).then(res => {
-                arr.forEach(element => {
-                    _self.$common.code2Text(element, "knfs", res[0]);
-                    _self.$common.code2Text(element, "gtype", res[1]);
+                arr.forEach((item, index, arr) => {
+                    _self.$common.code2Text(item, "knfs", res[0]);
+                    _self.$common.code2Text(item, "gtype", res[1]);
+                    let nameIndex = getItem("name", item.name, returnArr);
+                    if (nameIndex == -1) {
+                        // 风险点不一致 向后新增
+                        // 深拷贝
+                        let obj = this.$common.deepClone(item);
+                        // 加入到其危险源
+                        obj.child = [this.$common.deepClone(item)];
+                        // 加入到其管控措施
+                        obj.child[0].child = [this.$common.deepClone(item)];
+                        // 推入数组
+                        returnArr.push(obj);
+                    } else if (nameIndex >= 0) {
+                        // 风险点一致 匹配危险源
+                        let widArr = returnArr[nameIndex].child;
+                        let deptnameIndex = getItem(
+                            "deptname",
+                            item.deptname,
+                            widArr
+                        );
+                        if (deptnameIndex >= 0) {
+                            // 危险源一致 匹配危险源
+                            if (!widArr[deptnameIndex].child) {
+                                widArr[deptnameIndex].child = [];
+                            }
+                            widArr[deptnameIndex].child.push(item);
+                        } else if (deptnameIndex == -1) {
+                            // 危险源不一致 向后新增
+                            let obj = this.$common.deepClone(item);
+                            obj.child = [this.$common.deepClone(item)];
+                            widArr.push(obj);
+                        }
+                    }
                 });
-                _self.rendering = arr;
+                return returnArr;
             });
         }
     },
--- src/components/risk/riskBoard.vue
+++ src/components/risk/riskBoard.vue
@@ -11,36 +11,50 @@
         </van-sticky>
         <ViewBox :postData="postData" ref="view" @getRendering="getRendering">
             <div slot="views">
-                <div
-                    class="wrapper"
-                    v-for="(item,index) in rendering"
-                    :key="index"
-                    @click="btnClick(item)"
-                >
+                <div class="wrapper" v-for="(item,index) in rendering" :key="index">
                     <div class="title">
                         <h4>{{index+1+'.'+item.wname}}</h4>
                     </div>
                     <div class="main">
-                        <div>
-                            <p>事故类型: {{item.knfsText}}</p>
-                        </div>
-                        <div>
-                            <p>潜在后果: {{item.qzhgText}}</p>
-                        </div>
-                        <div>
-                            <p>影响范围: {{item.yxfwText}}</p>
-                        </div>
-                        <div>
-                            <p>责任人: {{item.zrr?item.zrr:""}}</p>
-                        </div>
-                        <div>
-                            <p>环节或部位: {{item.part}}</p>
-                        </div>
-                        <div>
-                            <p>有效期: {{item.validity}}</p>
-                        </div>
-                        <div>
-                            <p>报告电话: {{item.reportphone}}</p>
+                        <div
+                            class="noFlex"
+                            v-for="(n,m) in item.child"
+                            :key="m"
+                            @click.stop="btnClick(n)"
+                        >
+                            <div>
+                                <p>
+                                    <span class="main_title bold">{{"("+(m+1)+")"}}</span>
+                                    <span class="main_val bold">{{n.deptname}}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>事故类型: {{n.knfsText}}</p>
+                            </div>
+                            <div>
+                                <p>潜在后果: {{n.qzhgText}}</p>
+                            </div>
+                            <div>
+                                <p>影响范围: {{n.yxfwText}}</p>
+                            </div>
+                            <div v-for="(key,val) in n.child" :key="val">
+                                <p>
+                                    <span class="main_title">{{key.gtypeText+":"}}</span>
+                                    <span class="main_val">{{key.gname}}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>责任人: {{n.zrr?n.zrr:""}}</p>
+                            </div>
+                            <div>
+                                <p>环节或部位: {{n.part}}</p>
+                            </div>
+                            <div>
+                                <p>有效期: {{n.validity}}</p>
+                            </div>
+                            <div>
+                                <p>报告电话: {{n.reportphone}}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -86,25 +100,19 @@ export default {
     methods: {
         // 处理请求的数据
         getRendering(arr) {
-            let _self = this;
-            if (_self.knfs != "") {
-                arr.forEach(element => {
-                    _self.$common.code2Text(element, "knfs", _self.knfs);
-                    _self.$common.code2Text(element, "yxfw", _self.yxfw);
-                    _self.$common.code2Text(element, "qzhg", _self.qzhg);
-                });
-            }
-            _self.rendering = arr;
+            this.rendering = this.setRes(arr);
         },
         getComboList() {
             Promise.all([
                 this.$common.comboList({ sourcename: "KNFS" }),
                 this.$common.comboList({ sourcename: "YXFWEI" }),
-                this.$common.comboList({ sourcename: "QZHG" })
+                this.$common.comboList({ sourcename: "QZHG" }),
+                this.$common.comboList({ sourcename: "GKCSLX" })
             ]).then(res => {
                 this.knfs = res[0];
                 this.yxfw = res[1];
                 this.qzhg = res[2];
+                this.gtype = res[3];
             });
         },
         btnClick(obj) {
@@ -114,6 +122,53 @@ export default {
         getCompany(v) {
             this.postData.obj["bean.param"] = v.id;
             this.clearData();
+        },
+        // 设置返还参数
+        setRes(arr) {
+            let _self = this;
+            let returnArr = [];
+            function getItem(key, val, arr) {
+                return arr.findIndex(item => item[key] == val);
+            }
+            arr.forEach((item, index, arr) => {
+                _self.$common.code2Text(item, "knfs", _self.knfs);
+                _self.$common.code2Text(item, "yxfw", _self.yxfw);
+                _self.$common.code2Text(item, "qzhg", _self.qzhg);
+                _self.$common.code2Text(item, "gtype", _self.gtype);
+                let wnameIndex = getItem("wname", item.wname, returnArr);
+                if (wnameIndex == -1) {
+                    // 风险点不一致 向后新增
+                    // 深拷贝
+                    let obj = this.$common.deepClone(item);
+                    // 加入到其危险源
+                    obj.child = [this.$common.deepClone(item)];
+                    // 加入到其管控措施
+                    obj.child[0].child = [this.$common.deepClone(item)];
+                    // 推入数组
+                    returnArr.push(obj);
+                } else if (wnameIndex >= 0) {
+                    // 风险点一致 匹配危险源
+                    let widArr = returnArr[wnameIndex].child;
+                    let deptnameIndex = getItem(
+                        "deptname",
+                        item.deptname,
+                        widArr
+                    );
+                    if (deptnameIndex >= 0) {
+                        // 危险源一致 匹配危险源
+                        if (!widArr[deptnameIndex].child) {
+                            widArr[deptnameIndex].child = [];
+                        }
+                        widArr[deptnameIndex].child.push(item);
+                    } else if (deptnameIndex == -1) {
+                        // 危险源不一致 向后新增
+                        let obj = this.$common.deepClone(item);
+                        obj.child = [this.$common.deepClone(item)];
+                        widArr.push(obj);
+                    }
+                }
+            });
+            return returnArr;
         },
         // 清空数据 重新加载
         clearData() {
